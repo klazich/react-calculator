8c0cd20bf79ee2beba1f00345b9a195fd19283f6
Kevin Lazich <dev@kevinlazich.com>
2019-03-12 15:14:20 -0400

    chore: add component protos

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
+
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    {children}
+  </ThemeProvider>
+)
