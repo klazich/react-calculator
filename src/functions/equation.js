fbf43e98381a4cc30530cff827ba02fcec27c3a8
Kevin Lazich <dev@kevinlazich.com>
2019-04-29 18:39:55 -0400

    chore: Input checking for equation

--- src/functions/equation.js
+++ src/functions/equation.js
@@ -1,8 +1,20 @@
-const appendToEquation = input => equation => [...equation, input]
+import { is } from './functions'
+
+const append = x => arr => [...arr, x]
+const appendIf = test => x => arr => (test(arr) ? append(x)(arr) : arr)
+
+const canAppendOperand = equation => equation.length % 2 === 0
+const canAppendOperator = equation => equation.length % 2 !== 0
+
+const appendOperand = appendIf(canAppendOperand)
+const appendOperator = appendIf(canAppendOperator)
+
+const appendToEquationIfCan = input =>
+  is.operator(input) ? appendOperator(input) : appendOperand(input)
 
 export const updateEquation = input => state => ({
   ...state,
-  equation: appendToEquation(input)(state.equation),
+  equation: appendToEquationIfCan(input)(state.equation),
 })
 
 export const resetEquation = (init = []) => state => ({
