e8c161e2601993165d4d250a8cbecb192fe0832c
Kevin Lazich <dev@kevinlazich.com>
2019-04-29 17:19:31 -0400

    style: Hide overflow and clip text

--- src/components/Display/InputScreen.js
+++ src/components/Display/InputScreen.js
@@ -5,7 +5,15 @@ import Screen from './Screen'
 
 const InputScreen = ({ children }) => (
   <Card boxShadow="0 2px 10px rgba(0, 0, 0, 0.2)">
-    <Screen my={2} fontWeight={600} fontSize={6}>
+    <Screen
+      my={2}
+      fontWeight={600}
+      fontSize={6}
+      css={{
+        overflow: 'hidden',
+        textOverflow: 'clip',
+      }}
+    >
       {children}
     </Screen>
   </Card>
