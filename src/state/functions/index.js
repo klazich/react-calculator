5c693b80185d3e14afe1d56d44625555b89f63cc
Kevin Lazich <dev@kevinlazich.com>
2019-07-22 12:01:46 -0400

    chore: Remove excessive commenting

--- src/state/functions/index.js
+++ src/state/functions/index.js
@@ -29,37 +29,13 @@ export const updateLast = type => state => ({
 
 const pipe = (...fns) => x => fns.reduce((a, f) => f(a), x)
 
-/**
- * The state object
- * @typedef {Object} State
- * @prop {string} digits - The input digits
- * @prop {string} didExecute - True if the last input was '='
- * @prop {(number|string)[]} equation - The accumulated operand and operator inputs to be solved
- * @prop {State['equation'][]} history - Previously solved equations
- * @prop {string|null} last - The action previously dispatched
- */
-/**
- * State updater
- * @typedef {function} StateUpdate
- * @param {State} state - The current state object
- * @returns {State} The new state object
- */
-
 // Instructions for digit, decimal or backspace calculator inputs. ↤, ., 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-/**
- * @param {string} digit - The input value of the pressed digit key
- * @return {(state: State) => State} A function that takes a state object and returns a new state
- */
 export const inputDigit = digit => state =>
   pipe(
     updateDigits(digit) // update the state property 'digits' with 'digit'
   )(state)
 
-/**
- * @param {string} digit - The input value of the pressed digit key
- * @return {(state: State) => State} A function that takes a state object and returns a new state
- */
 export const inputDigitPostExec = digit => state =>
   pipe(
     resetEquation(),
@@ -68,10 +44,6 @@ export const inputDigitPostExec = digit => state =>
 
 // Instructions for operator calculator inputs. ÷, ×, -, +
 
-/**
- * @param {string} operator - The input value of the pressed operator key
- * @returns {(state: State) => State} A function that takes a state object and returns a new state
- */
 export const inputOperator = operator => state =>
   pipe(
     updateEquation(+state.digits),
@@ -79,10 +51,6 @@ export const inputOperator = operator => state =>
     resetDigits()
   )(state)
 
-/**
- * @param {string} operator - The input value of the pressed operator key
- * @return {(state: State) => State} A function that takes a state object and returns a new state
- */
 export const inputOperatorPostExec = operator => state =>
   pipe(
     resetEquation(),
@@ -92,9 +60,6 @@ export const inputOperatorPostExec = operator => state =>
 
 // Instructions for equal calculator input. =
 
-/**
- * @return {(state: State) => State} A function that takes a state object and returns a new state
- */
 export const inputExecute = () => state =>
   pipe(
     updateEquation(+state.digits),
@@ -103,13 +68,7 @@ export const inputExecute = () => state =>
     didJustExecute()
   )(state)
 
-/**
- * @return {(state: State) => State} A function that takes a state object and returns a new state
- */
-export const inputExecutePostExec = () => state =>
-  pipe(
-    didJustExecute() //
-  )(state)
+export const inputExecutePostExec = () => state => pipe(didJustExecute())(state)
 
 // Instructions when an equation is clicked in the history pane.
 
